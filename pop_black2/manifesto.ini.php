3be35c012631a2e06debf9bcbe6f225fa9ddecb5
mambax7 <mambax7@gmail.com>
2015-05-05 09:24:55 -0400

    Original files

---
+++ pop_black2/manifesto.ini.php
@@ -0,0 +1,21 @@
+<?php
+/**
+
+[Manifesto]
+Name="BCOOL THEME POP BLACK2"
+Depends=Legacy_RenderSystem
+Url="http://2bcool.net/"
+Version="1.0"
+
+[Theme]
+RenderSystem=Legacy_RenderSystem
+Format="XOOPS2 Legacy Style"
+Author=Masanao Takahashi @ BCOOL Inc.
+ScreenShot="screenshot.png"
+Description="2 or 3 columns variable width style theme"
+W3C=
+
+License="Attribution-NonCommercial-ShareAlike 2.1 Japan"
+
+*/
+?>
